01c4ec5d2276b92511febc8283ae98b6fbc3d41b
iben12 <iben@iben.hu>
2017-03-06 10:08:29 +0100

    Merge fix.

--- src/Services/Boxcar.php
+++ src/Services/Boxcar.php
@@ -8,19 +8,12 @@ Class Boxcar {
     
     function __construct()
     {
-<<<<<<< HEAD:src/boxcar.php
         $this->getConfig();
-        $this->token = $this->config->token;
     }
 
     private function getConfig() {
-        $config = file_get_contents(__DIR__ . '/../config.json');
-        $config = json_decode($config, true);
-        $this->config = $config['boxcar'];
-=======
         $config = require(__DIR__.'/../config.php');
         $this->token = $config["boxcar"]["token"];
->>>>>>> dev:src/Services/Boxcar.php
     }
 
     public function push($title, $body) {
@@ -34,17 +27,10 @@ Class Boxcar {
                     "notification[title]" => $title,
                     "notification[long_message]" => $body,
                     "notification[sound]" => "beep-soft",
-<<<<<<< HEAD:src/boxcar.php
-                )
-            )
-        );
-        $ret = curl_exec($push);
-=======
                 ]
             ]
         );
         curl_exec($push);
->>>>>>> dev:src/Services/Boxcar.php
         curl_close($push);
     }
 }
